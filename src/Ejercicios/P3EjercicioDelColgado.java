b64da6ff516462f0e7f12c70a9191f787a85abaf
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-11-15 17:23:48 +0100

    Comit de prints de las reglas

--- src/Ejercicios/P3EjercicioDelColgado.java
+++ src/Ejercicios/P3EjercicioDelColgado.java
@@ -1,11 +1,33 @@
 package Ejercicios;
 
+import java.util.Scanner;
 public class P3EjercicioDelColgado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner teclado = new Scanner(System.in);
 		
+		
+		System.out.println(" ____________________________________________");
+		System.out.println("|                                            |");
+		System.out.println("|      Bienvenido al Juego del Colgado       |");
+		System.out.println("|                                            |");
+		
+		
+		
+		
+		System.out.println(" ____________________________________________");
+		System.out.println("|                                            |");
+		System.out.println("|    Explicacion de las reglas del juego     |");
+		System.out.println("|                                            |");
+		
+		
+		System.out.println("Introduce cuantos jugadores quieren jugar");
+		
+		for (int i = 0; i < args.length; i++) {
+			
+		}
 		
 	}
 
