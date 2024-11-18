8399c5182e901102c5d01456caa50a3bca10d2ce
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-11-18 16:05:57 +0100

    Array Jugadores
    Creo una Array de jugadores donde se introducen x jugadores para saber cuantos juegan ya que el minimo son 2 jugadores i el maximo son 6 jugadores

--- src/Ejercicios/P3EjercicioDelColgado.java
+++ src/Ejercicios/P3EjercicioDelColgado.java
@@ -8,26 +8,38 @@ public class P3EjercicioDelColgado {
 
 		Scanner teclado = new Scanner(System.in);
 		
+		int [] Arrayjugadores; // Creamos la array para los jugadores que se introduzcan los que juegan
+		int jugadores = 0 ;
+		
 		
 		System.out.println(" ____________________________________________");
 		System.out.println("|                                            |");
 		System.out.println("|      Bienvenido al Juego del Colgado       |");
-		System.out.println("|                                            |");
+		System.out.println("|____________________________________________|");
+	
 		
 		
+		System.out.println(" ______________________________________________");
+		System.out.println("|                                              |");
+		System.out.println("|   Introduce cuantos jugadores quieren jugar  |");
+		System.out.println("|______________________________________________|");
 		
+		int Njugadores = teclado.nextInt();
 		
-		System.out.println(" ____________________________________________");
-		System.out.println("|                                            |");
-		System.out.println("|    Explicacion de las reglas del juego     |");
-		System.out.println("|                                            |");
-		
-		
-		System.out.println("Introduce cuantos jugadores quieren jugar");
-		
-		for (int i = 0; i < args.length; i++) {
+		while (Njugadores < 2 || Njugadores > 6) {
+			
+			System.out.println(" _____________________________________________________________");
+			System.out.println("|                                                             |");
+			System.out.println("|           El Minimo de jugadores son: 2 Jugadores           |");
+			System.out.println("|                                                             |");
+			System.out.println("|           El Maximo de jugadores son: 6 Jugadores           |");
+			System.out.println("|_____________________________________________________________|");
 			
 		}
+		
+		Arrayjugadores = new int [Njugadores];
+		
+		
 		
 	}
 
