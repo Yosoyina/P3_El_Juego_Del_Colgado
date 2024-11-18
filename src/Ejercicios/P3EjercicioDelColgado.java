96e9194069e263ad2ac3f96849c831bc38352c82
Carlos <131690161+Yosoyina@users.noreply.github.com>
2024-11-18 17:18:24 +0100

    String Array Nombres
    En esta parte he hecho los mismo que las variables int pero en los strings para que tantos jugadores se la ponga un nombre, ademas de que las variables la he creado arriba

--- src/Ejercicios/P3EjercicioDelColgado.java
+++ src/Ejercicios/P3EjercicioDelColgado.java
@@ -9,8 +9,7 @@ public class P3EjercicioDelColgado {
 		Scanner teclado = new Scanner(System.in);
 		
 		int [] Arrayjugadores; // Creamos la array para los jugadores que se introduzcan los que juegan
-		int jugadores = 0 ;
-		
+		int Njugadores = 0; // Variable del Numero de Jugadores que van a jugar
 		
 		System.out.println(" ____________________________________________");
 		System.out.println("|                                            |");
@@ -24,20 +23,40 @@ public class P3EjercicioDelColgado {
 		System.out.println("|   Introduce cuantos jugadores quieren jugar  |");
 		System.out.println("|______________________________________________|");
 		
-		int Njugadores = teclado.nextInt();
+		Njugadores = teclado.nextInt(); // introduccir cuantos jugadores van a jugar
 		
+		// Un bucle para que introduces del min 2 jugadores y el maximo 6 jugadores
 		while (Njugadores < 2 || Njugadores > 6) {
 			
 			System.out.println(" _____________________________________________________________");
 			System.out.println("|                                                             |");
 			System.out.println("|           El Minimo de jugadores son: 2 Jugadores           |");
+			System.out.println("|_____________________________________________________________|");
 			System.out.println("|                                                             |");
 			System.out.println("|           El Maximo de jugadores son: 6 Jugadores           |");
 			System.out.println("|_____________________________________________________________|");
 			
+			Njugadores = teclado.nextInt(); //Volver a introduccir de nuevo los jugadores 
 		}
 		
-		Arrayjugadores = new int [Njugadores];
+		Arrayjugadores = new int [Njugadores]; // De la arrayjugadores se introduccen los jugadores cuantos van a jugar i se crean esas arrays
+		
+		String [] NomJugadores; // Una array de jugadores pero para las letras
+		NomJugadores = new String [Njugadores]; // Coje el numero de jugadores y de tantos jugadores se pone el nombre de cada jugador
+		
+		for (int i = 0; i < NomJugadores.length; i++) {
+			
+			System.out.println(" ______________________________________________");
+			System.out.println("|                                              |");
+			System.out.println("|   Introduce el nombre de cada Jugador:       |");
+			System.out.println("|______________________________________________|");
+			
+			NomJugadores[i] = teclado.next(); // se introducce el nombre de los jugadores indicados
+			
+		}
+		
+		
+		
 		
 		
 		
